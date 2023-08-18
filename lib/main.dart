436085ae900c03c4ecb14038af5c0252fa62944c
os <kkeval304@gmail.com>
2023-08-18 15:53:04 +0530

    hhgdhg

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:task_5/screen/calc_screen.dart';
+import 'package:task_5/screen/dyn1_screen.dart';
+import 'package:task_5/screen/dyn_screen.dart';
+import 'package:task_5/screen/icons1_screen.dart';
+import 'package:task_5/screen/iconscolor_screen.dart';
+import 'package:task_5/screen/im_screen.dart';
+import 'package:task_5/screen/map_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:iconscolors(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
+
+
